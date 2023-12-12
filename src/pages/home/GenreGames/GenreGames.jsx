3587ce6edac05ddf55cf7875b988282417b3f371
Mauricio Rakoto <rakoto.mauricio@gmail.com>
2023-12-12 13:22:08 +0100

    Show Genre

--- src/pages/home/GenreGames/GenreGames.jsx
+++ src/pages/home/GenreGames/GenreGames.jsx
@@ -1,55 +1,59 @@
-import React from 'react'
+
 import '../../../assets/css/pages/home/genre-games.css'
 
+import React, { useEffect, useState } from "react"
+
 const GenreGames = () => {
+  const [genres, setGenres] = useState([])
+
+  const fetchData = () => {
+    fetch("http://localhost:8000/genres")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setGenres(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="genres-games">
         <div className="title">
             <h4>Genres</h4>
         </div>
-        <div className="items">
-            <div className="item">
-                <div className="image">
-                    <img src="http://localhost:8085/images/2023 BMW CAR  _ Car aesthetic _ Cool cars _ car wallpapers.jfif" alt=""/>
-                </div>
-                <div className="infos">
-                    <div className="name">
-                        <h3>Actions</h3>
+        
+        {genres.length > 0 && (
+            <div className='items'>
+            {genres.map(genre => (
+                <div className="item">
+                    <div className="image">
+                        <img src="http://localhost:8085/images/2023 BMW CAR  _ Car aesthetic _ Cool cars _ car wallpapers.jfif" alt=""/>
                     </div>
-                    <div className="nombres">
-                        <span>100</span>
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <div className="image">
-                    <img src="http://localhost:8085/images/téléchargement (1).jfif" alt=""/>
-                </div>
-                <div className="infos">
-                    <div className="name">
-                        <h3>Sports</h3>
-                    </div>
-                    <div className="nombres">
-                        <span>50</span>
+                    <div className="infos">
+                        <div className="name">
+                            <h3>{genre.name}</h3>
+                        </div>
+                        <div className="nombres">
+                            <span>100</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="item">
-                <div className="image">
-                    <img src="http://localhost:8085/images/games/mario/Felt Empty Without Luigi _ Mario's Flat Butt.jfif" alt=""/>
-                </div>
-                <div className="infos">
-                    <div className="name">
-                        <h3>Arcades</h3>
-                    </div>
-                    <div className="nombres">
-                        <span>200</span>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
+      )}
+            
+
+        
     </div>
   )
 }
+
+
+
+
 
 export default GenreGames
